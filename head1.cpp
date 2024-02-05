544b3f56a9868ce1f4b5514236087f663a8a64b9
JuneSunAt7 <jprogram@bk.ru>
2024-02-05 14:26:08 +0300

    header def for cpp and h file

---
+++ head1.cpp
@@ -0,0 +1,13 @@
+#include "head1.h"
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int Calculate::add(int a, int b){
+    return a + b;
+}
+int Calculate::mult(int x, int y){
+    return x*y;
+}
---
+++ head1.h
@@ -0,0 +1,13 @@
+int sum(int a, int b);
+
+class Calculate{
+
+    public:
+        int add(int a, int b);
+        int mult(int x, int y);
+};
+
+class Filesystem{
+    public:
+        
+}
