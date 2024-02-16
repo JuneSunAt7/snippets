d022341d3047b689701f995048598b21b58b65bd
JuneSunAt7 <jprogram@bk.ru>
2024-02-16 23:10:55 +0300

    clean and create threads template

--- folder_template.sh
+++ folder_template.sh
@@ -6,6 +6,8 @@ mkdir $folder_name
 cd $folder_name 
 echo "# Example for ${user_folder_name} and compile with g++ " > README.md
 mkdir "src"
+cd "src"
+echo "#include <iostream>  int main(){}" > main.cpp
 
 if [ $? -eq 0 ]; then echo "Папка успешно создана" 
 else echo "Ошибка при создании папки" fi
---
+++ threads_snippets/README.md
@@ -0,0 +1,1 @@
+# Example for threads and compile with g++ 
